a27ee70d03d0423123bf06cf407c14082db105b4
Joaquín Lorenzo <68236411+joa-byte@users.noreply.github.com>
2025-06-09 12:59:26 -0300

    feat: register image endpoint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TranscriptionController } from './transcription/transcription.controller';
 import { TranscriptionService } from './transcription/transcription.service';
+import { ImageController } from './image_controller';
+import { ImageService } from './image.service';
 
 @Module({
-  controllers: [TranscriptionController],
-  providers: [TranscriptionService],
+  controllers: [TranscriptionController, ImageController],
+  providers: [TranscriptionService, ImageService],
 })
 export class AppModule {}
